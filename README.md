8e05d79146a8ea669d800ead1d9b9fafcb8a811d
Bálint Aradi <baradi09@gmail.com>
2012-11-02 21:38:52 +0100

    Adding Readme, updateing setup.py

---
+++ README.md
@@ -0,0 +1,14 @@
+Human-friendly structured data (HSD)
+====================================
+
+The human-friendly structured data is a configuration markup language with a
+light easy to read and easy to type syntax, making it ideal for config files
+which must be created and edited by humans (such as input files for scientific
+programs). It allows arbitrary number of nesting levels and can be two-way
+mapped onto a subset of XML.
+
+This project provides utilites for parsing such data, building a tree structure
+out of it and to query it.
+
+Currently only a Python implementation is available, but other languages should
+follow later.
--- python/setup.py
+++ python/setup.py
@@ -1,41 +1,41 @@
-#!/usr/bin/env python3.1
+#!/usr/bin/env python3.2
 from distutils.core import setup
 
 setup(name="hsd",
       version="0.1",
       description="Parsing and processing framework for human-friendly "
         "structured data",
-      author="Dennis Bredemeier, Bálint Aradi",
-      author_email="dennis.bredemeier@gmail.com, baradi07@gmail.com",
-      url="http://code.google.com/p/hsdparser/",
-      license="MIT",
+      author="Bálint Aradi, Dennis Bredemeier",
+      author_email="baradi09@gmail.com",
+      url="http://bitbucket.org/aradi/hsdparser",
+      license="BSD",
       platforms="platform independent",
       package_dir={"": "src"},
       packages=["hsd", "hsdnum", ],
       scripts=[],
-      data_files=[("share/doc/hsd", ["LICENSE",])],
+      data_files=[],
       classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
         "Intended Audience :: Science/Research",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
-        "Topic :: Scientific/Engineering",
+        "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       long_description="""
-Parsing and processing framework for human-friendly structured data
--------------------------------------------------------------------
+Human-friendly structured data (HSD)
+====================================
 
-The human-friendly structured data is a markup language with a light syntax,
-making it ideal for config files, which need arbitrary number of nesting levels.
-It can be two-way mapped onto a subset of XML.
+The human-friendly structured data is a configuration markup language with a
+light easy to read and easy to type syntax, making it ideal for config files
+which must be created and edited by humans (such as input files for scientific
+programs). It allows arbitrary number of nesting levels and can be two-way
+mapped onto a subset of XML.
 
-This package provides utilites for parsing such data, converting it to an
-XML-tree (using etree) and to query it.
-
-Requires Python 3.1 or later.
+This project provides utilites for parsing such data, building a tree structure
+out of it and to query it.
 """
      )
